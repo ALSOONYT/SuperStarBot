f708a2df00ce98b38e4a51998758e368e6015207
AJBotsss <162437079+AJBotsss@users.noreply.github.com>
2024-03-10 15:04:59 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -44,7 +44,6 @@ async def start(client, message):
               
             InlineKeyboardButton("✨ Buy Subsccription ✨", callback_data="Buy Subscription")
         ]]
-        m = await message.reply_text("YOUR TEXT HERE""𝖲𝖳𝖠𝖱𝖳𝖨𝖭𝖦 👀")
         await asyncio.sleep(2)
         await message.reply_photo(photo=random.choice(PICS), caption=START_MESSAGE.format(user=message.from_user.mention, bot=client.mention), reply_markup=InlineKeyboardMarkup(buttons), parse_mode=enums.ParseMode.HTML)
         return await m.delete()
@@ -83,7 +82,7 @@ async def start(client, message):
              
             InlineKeyboardButton("✨ Buy Subscription ✨", callback_data="Buy Subscription")      
         ]]
-        m = await message.reply_sticker("CAACAgUAAxkBAAEBvlVk7YKnYxIHVnKW2PUwoibIR2ygGAACBAADwSQxMYnlHW4Ls8gQHgQ")
+    m = await message.reply_text("𝑺𝑻𝑨𝑹𝑻𝑰𝑵𝑮 👀...")
         await asyncio.sleep(2)
         await message.reply_photo(photo=random.choice(PICS), caption=START_MESSAGE.format(user=message.from_user.mention, bot=client.mention), reply_markup=InlineKeyboardMarkup(buttons), parse_mode=enums.ParseMode.HTML)
         return await m.delete()
